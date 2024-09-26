f3f3f736b166d6907b37a2298d2c6b5ae722f984
Nighthawk500 <jafm0108@gmail.com>
2024-09-26 14:32:34 -0400

    context menu

--- app/src/main/java/com/zybooks/diceroller/MainActivity.java
+++ app/src/main/java/com/zybooks/diceroller/MainActivity.java
@@ -4,18 +4,23 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements RollLengthDialogFragment.OnRollLengthSelectedListener {
 
     public static final int MAX_DICE = 3;
 
+    private int mCurrentDie;
     private CountDownTimer mTimer;
     private Menu mMenu;
     private int mVisibleDice;
@@ -44,6 +49,14 @@ public class MainActivity extends AppCompatActivity
         mVisibleDice = MAX_DICE;
 
         showDice();
+
+        registerForContextMenu(mDiceImageViews[0]);
+
+        // Register context menus for all dice and tag each die
+        for (int i = 0; i < mDiceImageViews.length; i++) {
+            registerForContextMenu(mDiceImageViews[i]);
+            mDiceImageViews[i].setTag(i);
+        }
     }
 
     @Override
@@ -137,5 +150,34 @@ public class MainActivity extends AppCompatActivity
         for (int i = numVisible; i < MAX_DICE; i++) {
             mDiceImageViews[i].setVisibility(View.GONE);
         }
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+        mCurrentDie = (int) v.getTag();   // Which die is selected?
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.add_one) {
+            mDice[mCurrentDie].addOne();
+            showDice();
+            return true;
+        }
+        else if (item.getItemId() == R.id.subtract_one) {
+            mDice[mCurrentDie].subtractOne();
+            showDice();
+            return true;
+        }
+        else if (item.getItemId() == R.id.roll) {
+            rollDice();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
